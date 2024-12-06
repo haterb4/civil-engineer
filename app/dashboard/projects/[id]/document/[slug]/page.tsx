24cb331836a3a9f876adb26f8b525369650b7280
haterb4 <haterb2803@gmail.com>
2024-12-06 15:48:25 +0100

    config

--- app/dashboard/projects/[id]/document/[slug]/page.tsx
+++ app/dashboard/projects/[id]/document/[slug]/page.tsx
@@ -2,8 +2,8 @@
 
 import { useState, useEffect, useRef } from 'react';
 import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import SignatureCanvas from 'react-signature-canvas';
 import { getDocumentById, addCommentToVersion, updateDocumentById } from '@/app/actions/documents/documentActions';
